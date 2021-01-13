2b88b34c5ca524ca7b485abae2ae6b0ac3af1b96
lucas <lcampbell0815@gmail.com>
2021-01-13 12:57:38 -0600

    added attack take damge

--- raygame/Entity.cpp
+++ raygame/Entity.cpp
@@ -1,1 +1,18 @@
 #include "Entity.h"
+
+Entity::Entity(float x, float y, float collisionRadius, const char* spriteFilePath, float maxSpeed) : Actor(x, y, collisionRadius, spriteFilePath, maxSpeed)
+{
+	
+}
+
+bool Entity::attack(Entity other)
+{
+	other.takeDamage(m_damage);
+	return true;
+}
+
+bool Entity::takeDamage(float totalDamage)
+{
+	m_health -= totalDamage;
+	return true;
+}
--- raygame/Entity.h
+++ raygame/Entity.h
@@ -4,15 +4,15 @@ class Entity : public Actor
 {
 public:
 	Entity() : Actor() {};
-	Entity(float x, float y, float collisionRadius, const char* spriteFilePath, float maxSpeed) : Actor(x, y, collisionRadius, spriteFilePath, maxSpeed) {};
+	Entity(float x, float y, float collisionRadius, const char* spriteFilePath, float maxSpeed);
 private:
 	
 public:
-	bool attack(Actor other);
+	bool attack(Entity other);
 	virtual bool takeDamage(float totalDamage);
 private:
+	bool m_isColliding = false;
 	float m_health;
 	float m_damage;
-	bool m_isColliding;
 };
 
