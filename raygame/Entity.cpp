e49ff6c96bc890e682d9db46aefc9e3f8389ddeb
lucas <lcampbell0815@gmail.com>
2021-01-13 15:31:47 -0600

    added health and damage

--- raygame/Entity.cpp
+++ raygame/Entity.cpp
@@ -1,8 +1,9 @@
 #include "Entity.h"
 
-Entity::Entity(float x, float y, float collisionRadius, const char* spriteFilePath, float maxSpeed) : Actor(x, y, collisionRadius, spriteFilePath, maxSpeed)
+Entity::Entity(float x, float y, float collisionRadius, const char* spriteFilePath, float maxSpeed,float health, float damage) : Actor(x, y, collisionRadius, spriteFilePath, maxSpeed)
 {
-	
+	m_health = health;
+	m_damage = damage;
 }
 
 bool Entity::attack(Entity other)
@@ -15,4 +16,9 @@ bool Entity::takeDamage(float totalDamage)
 {
 	m_health -= totalDamage;
 	return true;
+}
+
+void Entity::update(float deltaTime)
+{
+
 }
--- raygame/Entity.h
+++ raygame/Entity.h
@@ -4,12 +4,14 @@ class Entity : public Actor
 {
 public:
 	Entity() : Actor() {};
-	Entity(float x, float y, float collisionRadius, const char* spriteFilePath, float maxSpeed);
+	Entity(float x, float y, float collisionRadius, const char* spriteFilePath, float maxSpeed, float health, float damage);
 private:
 	
 public:
 	bool attack(Entity other);
 	virtual bool takeDamage(float totalDamage);
+
+	void update(float deltaTime) override;
 private:
 	bool m_isColliding = false;
 	float m_health;
