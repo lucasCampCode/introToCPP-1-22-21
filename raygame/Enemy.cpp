6c20830d730eef3fd1fb43da9973a59806cfb389
lucas <lcampbell0815@gmail.com>
2021-01-25 15:22:11 -0600

    made enemy have old tracking

--- raygame/Enemy.cpp
+++ raygame/Enemy.cpp
@@ -1,1 +1,42 @@
 #include "Enemy.h"
+#include "Player.h"
+Enemy::Enemy(float x, float y, float collisionRadius, const char* spriteFilePath, float maxSpeed, float health, float damage)
+	: Entity(x,y,collisionRadius,spriteFilePath,maxSpeed,health,damage)
+{
+
+}
+
+bool Enemy::checkTargetInSight(float maxAngle, float maxDistance)
+{
+	if (m_target == NULL)
+		return false;
+	MathLibrary::Vector2 direction = m_target->getWorldPosition() - getWorldPosition();
+
+	float distance = direction.getMagnitude();
+	float angle = (float)_CMATH_::acos(MathLibrary::Vector2::dotProduct(getVelocity(), direction));
+
+	if (angle < maxAngle && distance <= maxDistance)
+		return true;
+
+	return false;
+}
+
+void Enemy::onCollision(Actor* other)
+{
+	if(typeid(*other) == typeid(Player))
+		attack((Entity*)other);
+	other->setVelocity(getVelocity());
+}
+
+void Enemy::update(float deltaTime)
+{
+	if (checkTargetInSight(1, 3)) 
+	{
+
+	}
+	else
+	{
+
+	}
+
+}
--- raygame/Enemy.h
+++ raygame/Enemy.h
@@ -3,5 +3,16 @@
 class Enemy :
     public Entity
 {
+public:
+    Enemy() : Entity() {};
+    Enemy(float x, float y, float collisionRadius,const char* spriteFilePath,float maxSpeed,float health, float damage);
+public:
+    bool checkTargetInSight(float maxAngle, float maxDistance);
+    void setTarget(Actor* other) { m_target = other; }
+    void onCollision(Actor* other) override;
+    void update(float deltaTime) override;
+    
+private:
+    Actor* m_target;
 };
 
